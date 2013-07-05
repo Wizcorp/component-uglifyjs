8c0f013eb1e16d0de1ce2d3f71dee6070014ebb2
Ron Korving <rkorving@wizcorp.jp>
2013-07-05 18:40:10 +0900

    Now for Uglify 2 API.
    Also added gitignore and a readme.

---
+++ README.md
@@ -0,0 +1,46 @@
+# component-uglifyjs
+
+UglifyJS plugin for component-builder
+
+## Installing
+
+```bash
+npm install component-uglifyjs
+```
+
+## Usage
+
+To enable UglifyJS minification of your scripts, run the following code:
+
+``javascript
+var Builder = require('component-builder');
+var uglify = require('component-uglify');
+var fs = require('fs');
+
+var builder = new Builder(__dirname);
+
+builder.use(uglify);
+
+builder.build(function (error, build) {
+    if (error) {
+		throw error;
+	}
+
+    fs.writeFileSync('build/build.js', build.require + build.js);
+
+    if (build.css) {
+		fs.writeFileSync('build/build.css', build.css);
+	}
+});
+```
+
+Or from the command line:
+
+```bash
+component build --use component-uglify
+```
+
+## License
+
+MIT
+
--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 var fs = require('fs');
 var extname = require('path').extname;
-var jsp = require('uglify-js').parser;
-var pro = require('uglify-js').uglify;
-
-var parseStrictSemicolons = false;
-var mangle = true;
-var squeeze = true;
-var mangleOptions = undefined;
-var squeezeOptions = undefined;
-var gencodeOptions = undefined;
+var uglify = require('uglify-js');
 
 
 function jsFilter(fileName) {
@@ -29,60 +21,13 @@ exports = module.exports = function (builder) {
 		files.forEach(function (file) {
 			var path = builder.path(file);
 
-			var data = fs.readFileSync(path, 'utf8');
+			result = uglify.minify(path);
 
-			var ast = jsp.parse(data, parseStrictSemicolons); // parse code and get the initial AST
-
-			if (mangle) {
-				ast = pro.ast_mangle(ast, mangleOptions); // get a new AST with mangled names
-			}
-
-			if (squeeze) {
-				ast = pro.ast_squeeze(ast, squeezeOptions); // get an AST with compression optimizations
-			}
-
-			data = pro.gen_code(ast, gencodeOptions); // compressed code here
-
-			if (data) {
+			if (result) {
 				builder.removeFile('scripts', file);
-				builder.addFile('scripts', file, data);
+				builder.addFile('scripts', file, result.code);
 			}
 		});
 	});
 };
-
-
-exports.enableMangle = function (options) {
-	mangle = true;
-	mangleOptions = options;
-};
-
-exports.disableMangle = function () {
-	mangle = false;
-};
-
-exports.enableSqueeze = function () {
-	squeeze = true;
-};
-
-exports.disableSqueeze = function () {
-	squeeze = false;
-};
-
-exports.parseStrictSemicolons = function (enabled) {
-	parseStrictSemicolons = enabled;
-};
-
-exports.mangleOptions = function (options) {
-	mangleOptions = options;
-};
-
-exports.squeezeOptions = function (options) {
-	squeezeOptions = options;
-};
-
-exports.gencodeOptions = function (options) {
-	gencodeOptions = options;
-};
-
 
